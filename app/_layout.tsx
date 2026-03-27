42e2209014e9d7d663ce31f55897c792cc9f11e9
Logan <logan.loch02@gmail.com>
2026-03-26 22:30:15 -0500

    added dependency for react-query persistence created queryClient and asyncStoragePersister in lib/queryClient.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,34 @@
+import { asyncStoragePersister, queryClient } from "@/lib/queryClient";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { Stack } from "expo-router";
 import { AuthProvider } from '../contexts/AuthContext';
 import { ThemeProvider } from '../lib/ThemeContext';
 
+
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <ActionSheetProvider>
-        <AuthProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="job/[id]" options={{ title: 'Job Details', headerShown: true }} />
-          <Stack.Screen name="more/profiledetails" options={{ title: 'My Profile', headerShown: true }} />
-          <Stack.Screen name="more/schedule" options={{ title: 'My Schedule', headerShown: true }} />
-        </Stack>
-        </AuthProvider>
-      </ActionSheetProvider>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ 
+          persister: asyncStoragePersister,
+          maxAge: 1000 * 60 * 60 * 24, // 1 day
+          buster: 'v1', // Change this to invalidate old cache
+        }}
+      >
+        <ActionSheetProvider>
+          <AuthProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="job/[id]" options={{ title: 'Job Details', headerShown: true }} />
+            <Stack.Screen name="more/profiledetails" options={{ title: 'My Profile', headerShown: true }} />
+            <Stack.Screen name="more/schedule" options={{ title: 'My Schedule', headerShown: true }} />
+          </Stack>
+          </AuthProvider>
+        </ActionSheetProvider>
+      </PersistQueryClientProvider>
     </ThemeProvider>
   );
 }
